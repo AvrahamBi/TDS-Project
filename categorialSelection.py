97f4d4f053f5472f9afbb37104085990022a6ab3
AvrahamBi <avrahambarilan@gmail.com>
2022-02-11 13:38:46 +0200

    K functions done

--- categorialSelection.py
+++ categorialSelection.py
@@ -115,64 +115,6 @@ features, scores = showGraph(selector, features, 10)
 
 
 
-# def reduce(ds, method="k_best", k=3, p=10):
-#     dataSet = ds
-#     X = dataSet.data
-#     y = dataSet.target
-#     # Convert to categorical data by converting data to integers
-#     X = X.astype(int)
-#     # Two features with highest chi-squared statistics are selected
-#     if (method == "k_best"): chi2_features = SelectKBest(chi2, k=k)
-#     if (method == "percentile"): chi2_features = SelectPercentile(chi2,percentile=10)
-#
-#     X_kbest = chi2_features.fit_transform(X, y)
-#     # Reduced features
-#     print('Original feature number:', X.shape[1])
-#     print('Reduced feature number:', X_kbest.shape[1])
-
-
-
-#checkGraphs()
-
-#X, y = load_iris(return_X_y=True)
-#iris - load_iris()
-#digits = load_digits()
-# diabetes = load_diabetes()
-# wine = load_wine()
-#cancer = load_breast_cancer()
-# ds = [iris, digits, diabetes, wine, cancer]
-# for d in ds:
-#     check(d)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
